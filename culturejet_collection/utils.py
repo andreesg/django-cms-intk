473678b74cf45abf75aa0ce50736a55a7305703d
André Gonçalves <andreslb1@gmail.com>
2014-09-04 17:53:36 +0200

    get by language

--- culturejet_collection/utils.py
+++ culturejet_collection/utils.py
@@ -5,7 +5,7 @@ def get_subpage_content_from_page(page, language=None):
 
     children = page.get_children()
     for child in children:
-        plugins.append({'instance':child, 'plugins':child.placeholders.get(slot="content").get_plugins().order_by('-position').reverse()})
+        plugins.append({'instance':child, 'plugins':child.placeholders.get(slot="content").get_plugins(language).order_by('-position').reverse()})
 
     return plugins
         
