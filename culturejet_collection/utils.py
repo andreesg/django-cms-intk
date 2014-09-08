da0eede62e0b8c96803ca935ab7fe36ce5e6df93
André Gonçalves <andreslb1@gmail.com>
2014-09-08 17:35:44 +0200

    fix utils

--- culturejet_collection/utils.py
+++ culturejet_collection/utils.py
@@ -5,18 +5,20 @@ def get_subpage_content_from_page(page, language=None):
 
     children = page.get_children()
     for child in children:
-        child_plugins = child.placeholders.get(slot="content").get_plugins(language).order_by('-position').reverse()
-        
-        if len(child_plugins) > 0:
-            # TODO
-            try:
-                filer_plugin = child_plugins.filter(plugin_type='FilerImagePlugin')[0]
-                simpletext_plugin = child_plugins.filter(plugin_type='SimpleTextPlugin')[0]
-                picture = filer_plugin.get_plugin_instance()[0].image
-                description = simpletext_plugin
-                plugins.append({'instance':child, 'picture': picture, 'description': description})
-            except:
-                pass
+        if child.publication_date:
+            
+            child_plugins = child.placeholders.get(slot="content").get_plugins(language).order_by('-position').reverse()
+            
+            if len(child_plugins) > 0:
+                # TODO
+                try:
+                    filer_plugin = child_plugins.filter(plugin_type='FilerImagePlugin')[0]
+                    simpletext_plugin = child_plugins.filter(plugin_type='SimpleTextPlugin')[0]
+                    picture = filer_plugin.get_plugin_instance()[0].image
+                    description = simpletext_plugin
+                    plugins.append({'instance':child, 'picture': picture, 'description': description})
+                except:
+                    pass
 
     return plugins
-        
+
